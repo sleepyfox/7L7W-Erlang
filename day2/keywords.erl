56f7ad0ec3d35b095bd02142114a4c9df94afaed
Fox <sleepyfox@gmail.com>
2015-06-14 19:12:41 +0100

    changed null to false to make more Erlangy

--- day2/keywords.erl
+++ day2/keywords.erl
@@ -1,12 +1,12 @@
 -module(keywords).
 -include_lib("eunit/include/eunit.hrl").
 
-lookup([], _) -> null;
+lookup([], _) -> false;
 lookup(List, Word) ->
     element(2, hd(lists:filter(fun({X,_}) -> X == Word end, List))).
 
 empty_list_returns_null_test() ->
-    ?assertEqual(null, lookup([], word)).
+    ?assertEqual(false, lookup([], word)).
 
 can_find_word_in_list_of_one_item_test() ->
     Animal = [{cat, "a feline"}],
